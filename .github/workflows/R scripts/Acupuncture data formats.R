ae17334509b451bb615727f3c333bede344246a4
amydebrun <a.browne47@universityofgalway.ie>
2025-03-25 09:45:27 +0000

    few lmer models on acupuncture set, with and without imputation. comparison through forest plot

--- .github/workflows/R scripts/Acupuncture data formats.R
+++ .github/workflows/R scripts/Acupuncture data formats.R
@@ -28,3 +28,33 @@ acu_long <- acu_wide %>%
 #Acupuncture filtered for LOCF
 acu_filtered<- acu_long%>%
   filter(time != "3m")
+
+#MICE
+acu_mice <- mice(acu_wide, m = 5, method = 'pmm', seed = 123)
+acu_mice_data <- complete(data = acu_mice, action = "long", include = TRUE)
+
+acu_mice_data_long <- acu_mice_data %>%
+  pivot_longer(
+    cols = c('pk2', 'pk5'), 
+    names_to = c('pk_time'),
+    values_to = "pk_score"
+  ) %>%
+  pivot_longer(
+    cols = c('f2', 'f5'), 
+    names_to = c('freq_time'),
+    values_to = "freq_score",
+    names_repair = "unique"
+  ) %>%
+  filter((pk_time == "pk2" & freq_time == "f2") | (pk_time == "pk5" & freq_time == "f5")) %>%
+  mutate(
+    time = case_when(
+      pk_time == "pk2" ~ "3m",
+      pk_time == "pk5" ~ "12m"
+    )
+  ) %>%
+  select(-c(pk_time, freq_time))%>%
+  group_by(.imp) %>%
+  mutate(.id = 1:n())
+
+
+acu_mi_obj_long <- as.mids(acu_mice_data_long)
