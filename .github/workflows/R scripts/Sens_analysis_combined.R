088e5712ec3ad96e5a087939ded298ff378b7c2a
amydebrun <a.browne47@universityofgalway.ie>
2025-06-03 20:44:53 +0100

    .

--- .github/workflows/R scripts/Sens_analysis_combined.R
+++ .github/workflows/R scripts/Sens_analysis_combined.R
@@ -47,28 +47,6 @@ delta_combined_cont_acu$estimand <- "Continuous time"
 delta_combined_cat_acu$estimand <- "Categorical time"
 delta_combined_cont_cat <- bind_rows(delta_combined_cont_acu, delta_combined_cat_acu)
 
-delta_combined_cont_cat_plot <- ggplot(delta_combined_cont_cat, aes(x = estimate, y = delta)) +
-  geom_point(size = 4, color = "#a80050", position = position_nudge(y = 0.15)) +
-  geom_errorbarh(aes(xmin = conf.low, xmax = conf.high), height = 0.4,
-                 position = position_nudge(y = 0.15)) +
-  geom_vline(xintercept = 0, linetype = "dashed", color = "red") +
-  facet_wrap(~ treatment) +
-  labs(
-    title = "δ-Adjustment Sensitivity Analysis: Continuous & Categorical estimand",
-    x = "Estimated Treatment Effect",
-    y = "Delta"
-  ) +
-  theme_minimal() +
-  theme(
-    strip.background = element_rect(fill = "lawngreen", color = "black"),  
-    panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
-    panel.background = element_rect(fill = "white", color = NA),
-    plot.background = element_rect(fill = "white", color = NA)
-  )
-
-
-
-
 delta_combined_cont_cat_plot <- ggplot(delta_combined_cont_cat, aes(x = estimate, y = delta, shape = estimand)) +
   geom_point(size = 4, color = "#a80050", position = position_nudge(y = 0.15)) +
   geom_errorbarh(aes(xmin = conf.low, xmax = conf.high), height = 0.4,
