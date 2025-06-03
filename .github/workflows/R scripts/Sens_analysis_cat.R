1d19c7f791fd6daabe1971faed2b96417a1a5702
amydebrun <a.browne47@universityofgalway.ie>
2025-06-03 14:12:39 +0100

    sensitivity analysis categorical fix

--- .github/workflows/R scripts/Sens_analysis_cat.R
+++ .github/workflows/R scripts/Sens_analysis_cat.R
@@ -2,7 +2,7 @@
 
 # Acupuncture categorical treatment
 delta <- c(-5,-2,0,2,5)
-inlist <- c("sex", "age", "pk1")
+inlist <- c("group", "pk5", "pk1")
 pred_cat <- quickpred(acu_wide, minpuc = 0.5, include = inlist)
 imp.default_cat <- mice(acu_wide, m = 1, maxit = 1, predictorMatrix = pred_cat, seed = 123, print= FALSE)
 post_cat <- imp.default_cat$post
@@ -11,10 +11,8 @@ imp.all.undamped_cat <- vector("list", length(delta))
 for (i in 1:length(delta)) {
   d <- delta[i]
   cmd <- paste0(
-    "imp[[j]]$pk5 <- ifelse(imp[[j]]$group == 1 & is.na(data$pk5), imp[[j]]$pk5 + ", 
-    d, 
-    ", imp[[j]]$pk5)"
-  )
+    "idx <- which(data[,'group'] == 1 & is.na(data[,'pk5'])); ",
+    "imp[[j]]$pk5[idx] <- imp[[j]]$pk5[idx] + ",d)
   post_cat["pk5"] <- cmd
   imp_cat <- mice(acu_wide, pred = pred_cat, post = post_cat, maxit = 10,
                   seed = i * 22, print=FALSE)
@@ -65,9 +63,8 @@ imp.all.undamped_cat_placebo <- vector("list", length(delta))
 for (i in 1:length(delta)) {
   d <- delta[i]
   cmd <- paste0(
-    "imp[[j]]$pk5 <- ifelse(imp[[j]]$group == 0 & is.na(data$pk5), imp[[j]]$pk5 + ", 
-    d, 
-    ", imp[[j]]$pk5)"
+    "idx <- which(data[,'group'] == 0 & is.na(data[,'pk5'])); ",
+    "imp[[j]]$pk5[idx] <- imp[[j]]$pk5[idx] + ", d
   )
   post_cat["pk5"] <- cmd
   imp_cat <- mice(acu_wide, pred = pred_cat, post = post_cat, maxit = 10,
@@ -111,6 +108,8 @@ delta_result_cat_placebo_plot <- ggplot(delta_results_cat_placebo, aes(x = estim
 
 
 #categorical fishoil
+delta <- c(-10,-5,-2,0,2,5,10)
+inlist <- c("pain_base","time_contin", "vitdactive", "fishoilactive")
 pred_cat <- quickpred(vital_wide, minpuc = 0.5, include = inlist)
 imp.default_cat <- mice(vital_wide, m = 1, maxit = 1, predictorMatrix = pred_cat, seed = 123, print= FALSE)
 post_cat<- imp.default_cat$post
@@ -160,10 +159,11 @@ delta_results_cat_fishoil_plot <- ggplot(delta_results_cat_fishoil, aes(x = esti
 
 
 #categorical vitamin D
-
+delta <- c(-10,-5,-2,0,2,5,10)
+inlist <- c("pain_base","time_contin", "vitdactive", "fishoilactive")
 for (i in 1:length(delta)) {
   d <- delta[i]
-  cmd <- paste("imp[[j]]$pain_yr4 <- imp[[j]]$pain_yr4 +", d)
+  cmd <- paste("imp[[j]][,i] <- imp[[j]][,i] +", d)
   post_cat["pain_yr4"] <- cmd
   imp_cat <- mice(vital_wide, pred = pred_cat, post = post_cat, maxit = 10,
                   seed = i * 22, print=FALSE)
@@ -207,17 +207,24 @@ delta_results_cat_vitd_plot <- ggplot(delta_results_cat_vitd, aes(x = estimate, 
 
 
 #categorical vital placebo
-vital_wide$placebo<- ifelse(vital_long$fishoilactive == 0 & vital_long$vitdactive == 0, 1, 0)
+delta <- c(-10,-5,-2,0,2,5,10)
+inlist <- c("pain_base","time_contin", "vitdactive", "fishoilactive", "placebo", "pain_yr4")
+vital_wide$placebo<- ifelse(vital_wide$fishoilactive == 0 & vital_wide$vitdactive == 0, 1, 0)
 pred_cat <- quickpred(vital_wide, minpuc = 0.5, include = inlist)
 imp.default_cat <- mice(vital_wide, m = 1, maxit = 1, predictorMatrix = pred_cat, seed = 123, print= FALSE)
+imp.default_cat$method["pain_yr4"] <- "pmm"
+method <- imp.default_cat$method
 post_cat<- imp.default_cat$post
 imp.all.undamped_cat <- vector("list", length(delta))
 
 for (i in 1:length(delta)) {
   d <- delta[i]
-  cmd <- paste("imp[[j]]$pain_yr4 <- imp[[j]]$pain_yr4 +", d)
+  cmd <- paste0(
+    "imp[[j]]$pain_yr4[!is.na(imp[[j]]$pain_yr4)] <- ",
+    "imp[[j]]$pain_yr4[!is.na(imp[[j]]$pain_yr4)] + ", d
+  )
   post_cat["pain_yr4"] <- cmd
-  imp_cat <- mice(vital_wide, pred = pred_cat, post = post_cat, maxit = 10,
+  imp_cat <- mice(vital_wide, method = method, pred = pred_cat, post = post_cat, maxit = 10,
                   seed = i * 22, print=FALSE)
   imp.all.undamped_cat[[i]] <- imp_cat
 }
