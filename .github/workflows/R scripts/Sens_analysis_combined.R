c71fb16937e957634382ecf462bf32d3a2544481
amydebrun <a.browne47@universityofgalway.ie>
2025-06-09 22:25:10 +0100

    sensitivity analysis heat map for acu

--- .github/workflows/R scripts/Sens_analysis_combined.R
+++ .github/workflows/R scripts/Sens_analysis_combined.R
@@ -164,3 +164,157 @@ SA_combined_vital_all_plot <- ggplot(SA_combined_vital_all, aes(x = estimate, y 
     plot.background = element_rect(fill = "white", color = NA),
     legend.position = "none"
   )
+
+
+
+
+
+
+
+## heatmap for acupuncture
+
+cat_placebo <- delta_combined_acu_cont_cat %>%
+  filter(estimand == "Categorical time", treatment == "Placebo")
+cat_acu <- delta_combined_acu_cont_cat %>%
+  filter(estimand == "Categorical time", treatment == "Acupuncture")
+cont_placebo <- delta_combined_acu_cont_cat %>%
+  filter(estimand == "Continuous time", treatment == "Placebo")
+cont_acu <- delta_combined_acu_cont_cat %>%
+  filter(estimand == "Continuous time", treatment == "Acupuncture")
+cat_heatmap_data <- tidyr::expand_grid(
+  delta_1 = cat_placebo$delta_acu,  
+  delta_2 = cat_acu$delta_acu       
+) %>%
+  left_join(cat_placebo %>% select(delta_1 = delta_acu, placebo_est = estimate), by = "delta_1") %>%
+  left_join(cat_acu %>% select(delta_2 = delta_acu, acu_est = estimate), by = "delta_2") %>%
+  mutate(
+    treatment_effect = acu_est - placebo_est,
+    estimand = "Categorical time"
+  )
+cont_heatmap_data <- tidyr::expand_grid(
+  delta_1 = cont_placebo$delta_acu,
+  delta_2 = cont_acu$delta_acu
+) %>%
+  left_join(cont_placebo %>% select(delta_1 = delta_acu, placebo_est = estimate), by = "delta_1") %>%
+  left_join(cont_acu %>% select(delta_2 = delta_acu, acu_est = estimate), by = "delta_2") %>%
+  mutate(
+    treatment_effect = acu_est - placebo_est,
+    estimand = "Continuous time"
+  )
+
+range_est_cat <- range(cat_heatmap_data$treatment_effect, na.rm = TRUE)
+midpoint_cat <- mean(range_est_cat)
+spread_cat <- max(abs(range_est_cat - midpoint_cat))
+
+cat_heatmap_acu_plot<- ggplot(cat_heatmap_data, aes(x = delta_1, y = delta_2, fill = treatment_effect)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient2(
+    low = "#a80050", mid = "white", high = "lawngreen",
+    midpoint = midpoint_cat,
+    limits = c(midpoint_cat - spread_cat, midpoint_cat + spread_cat)
+  ) +
+  labs(
+    title = "Heatmap of categorical SA acupuncture",
+    x = "Control Delta ",
+    y = "Acupuncture Delta",
+    fill = "Treatment Effect"
+  ) +
+  theme_minimal() +
+  theme(panel.grid = element_blank())
+
+range_est_cont <- range(cont_heatmap_data$treatment_effect, na.rm = TRUE)
+midpoint_cont <- mean(range_est_cont)
+spread_cont <- max(abs(range_est_cont - midpoint_cont))
+
+cont_heatmap_acu_plot<-ggplot(cont_heatmap_data, aes(x = delta_1, y = delta_2, fill = treatment_effect)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient2(
+    low = "#a80050", mid = "white", high = "lawngreen",
+    midpoint = midpoint_cont,
+    limits = c(midpoint_cont - spread_cont, midpoint_cont + spread_cont)
+  ) +
+  labs(
+    title = "Heatmap continuous acu",
+    x = "Control Delta ",
+    y = "Acupuncture Delta",
+    fill = "Treatment Effect"
+  ) +
+  theme_minimal() +
+  theme(panel.grid = element_blank())
+
+
+
+#vital heatmap fishoil
+
+cat_placebo <- SA_combined_vital_all %>%
+  filter(estimand == "Categorical", treatment == "Control")
+cat_fishoil <- SA_combined_vital_all %>%
+  filter(estimand == "Categorical", treatment == "Fish Oil")
+cont_placebo <- SA_combined_vital_all %>%
+  filter(estimand == "Continuous time", treatment == "Control")
+cont_fishoil <- SA_combined_vital_all %>%
+  filter(estimand == "Continuous time", treatment == "Fish Oil")
+cat_heatmap_data <- tidyr::expand_grid(
+  delta_1 = cat_placebo$delta_vital,  
+  delta_2 = cat_fishoil$delta_vital      
+) %>%
+  left_join(cat_placebo %>% select(delta_1 = delta_vital, placebo_est = estimate), by = "delta_1") %>%
+  left_join(cat_fishoil %>% select(delta_2 = delta_vital, fishoil_est = estimate), by = "delta_2") %>%
+  mutate(
+    treatment_effect = fishoil_est - placebo_est,
+    estimand = "Categorical"
+  )
+cont_heatmap_data <- tidyr::expand_grid(
+  delta_1 = cont_placebo$delta_vital,
+  delta_2 = cont_fishoil$delta_vital
+) %>%
+  left_join(cont_placebo %>% select(delta_1 = delta_vital, placebo_est = estimate), by = "delta_1") %>%
+  left_join(cont_fishoil %>% select(delta_2 = delta_vital, fishoil_est = estimate), by = "delta_2") %>%
+  mutate(
+    treatment_effect = fishoil_est - placebo_est,
+    estimand = "Continuous"
+  )
+
+range_est_cat <- range(cat_heatmap_data$treatment_effect, na.rm = TRUE)
+midpoint_cat <- mean(range_est_cat)
+spread_cat <- max(abs(range_est_cat - midpoint_cat))
+
+cat_heatmap_fishoil_plot<- ggplot(cat_heatmap_data, aes(x = delta_1, y = delta_2, fill = treatment_effect)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient2(
+    low = "#a80050", mid = "white", high = "lawngreen",
+    midpoint = midpoint_cat,
+    limits = c(midpoint_cat - spread_cat, midpoint_cat + spread_cat)
+  ) +
+  labs(
+    title = "Heatmap of categorical SA fishoil",
+    x = "Control Delta ",
+    y = "Fish Oil Delta",
+    fill = "Treatment Effect"
+  ) +
+  theme_minimal() +
+  theme(panel.grid = element_blank())
+
+range_est_cont <- range(cont_heatmap_data$treatment_effect, na.rm = TRUE)
+midpoint_cont <- mean(range_est_cont)
+spread_cont <- max(abs(range_est_cont - midpoint_cont))
+
+cont_heatmap_fishoil_plot<-ggplot(cont_heatmap_data, aes(x = delta_1, y = delta_2, fill = treatment_effect)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient2(
+    low = "#a80050", mid = "white", high = "lawngreen",
+    midpoint = midpoint_cont,
+    limits = c(midpoint_cont - spread_cont, midpoint_cont + spread_cont)
+  ) +
+  labs(
+    title = "Heatmap continuous fishoil",
+    x = "Control Delta ",
+    y = "Fish Oil Delta",
+    fill = "Treatment Effect"
+  ) +
+  theme_minimal() +
+  theme(panel.grid = element_blank())
+
+
+
+
