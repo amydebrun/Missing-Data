5b3b3bbf60e3adbd39a651ebd5ce12ab7484ae11
amydebrun <a.browne47@universityofgalway.ie>
2025-06-10 15:08:21 +0100

    .

--- .github/workflows/R scripts/Data_Summary.R
+++ .github/workflows/R scripts/Data_Summary.R
@@ -370,7 +370,7 @@ pain_yr4_vital_plot<-vital_wide %>%
   )
 
 ---------------------------
-save(pain_base_vital_plot, pain_yr1_vital_plot, pain_yr2_vital_plot, pain_yr3_vital_plot, pain_yr4_vital_plot,
+save(acu_var_table,pain_base_vital_plot, pain_yr1_vital_plot, pain_yr2_vital_plot, pain_yr3_vital_plot, pain_yr4_vital_plot,
      acu_pk1_plot, acu_pk2_plot, acu_pk5_plot, acu_sex_plot, acu_age_plot, sex_vital_plot, age_vital_plot, bmi_vital_plot, file="report_plots.RData")  
 
 
