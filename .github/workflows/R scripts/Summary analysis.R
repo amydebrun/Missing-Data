af245e89a43f87f2ec1c2b6e0a3cc4a1759c806b
amydebrun <a.browne47@universityofgalway.ie>
2025-06-23 21:35:29 +0100

    knit

--- .github/workflows/R scripts/Summary analysis.R
+++ .github/workflows/R scripts/Summary analysis.R
@@ -29,7 +29,7 @@ vital_summary <- vital_wide %>%
     missing = "ifany",
     missing_text = "Missing"
   ) 
-vital_summary<-as_flex_table(vital_summary)
+vital_summary <-as_gt(vital_summary)
 
 # Missing plot
 acu_miss_plot <- gg_miss_var(acu_wide) 
